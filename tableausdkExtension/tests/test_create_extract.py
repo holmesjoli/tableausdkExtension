8312365018bbc852cbe3b704796020d8c69fde5e
Joli Holmes <jh111@ad.rice.edu>
2019-04-12 09:29:19 -0500

    initial commit

---
+++ tableausdkExtension/tests/test_create_extract.py
@@ -0,0 +1,12 @@
+import unittest
+import pandas as pd
+
+from tableausdkExtension.create_extract import create_extract
+
+class CreateExtractTestClass(unittest.TestCase):
+ 
+    def __init__(self, *args, **kwargs):
+ 
+        super(CreateExtractTestClass, self).__init__(*args, **kwargs)
+
+
