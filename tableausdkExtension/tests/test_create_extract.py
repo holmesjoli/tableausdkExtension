7d17f70955b6bb4be87639f624baf05eeb10dc1e
Joli Holmes <jh111@ad.rice.edu>
2019-04-12 09:55:51 -0500

    tests that the files get created

--- tableausdkExtension/tests/test_create_extract.py
+++ tableausdkExtension/tests/test_create_extract.py
@@ -1,7 +1,8 @@
 import unittest
+import os
 import pandas as pd
 
-from tableausdkExtension.create_extract import create_extract
+from create_extract import create_extract
 
 class CreateExtractTestClass(unittest.TestCase):
  
@@ -9,4 +10,19 @@ class CreateExtractTestClass(unittest.TestCase):
  
         super(CreateExtractTestClass, self).__init__(*args, **kwargs)
 
+        self.filename = "test_file.hyper"
+        self.df = pd.DataFrame({"col1": [1,2,3,4], 
+                                "col2": ["a", "b", "c", "d"],
+                                "col3": [1.0, 2.0, 3.0, 4.0]})
 
+        self.col_types = {"col1": "INTEGER",
+                          "col2": "CHAR_STRING",
+                          "col3": "DOUBLE"}
+
+        create_extract(self.filename, self.df, self.col_types)
+
+    def test_create_extract(self):
+
+        fls = [self.filename, "DataExtract.log", "hyper_db"]
+        [self.assertTrue(fl in os.listdir()) for fl in fls]
+        [os.remove(fl) for fl in fls]
